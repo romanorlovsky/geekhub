b622a53fe2299a0afd447bc644a58b7c12c55fa4
roman.orlovskyi <roman.orlovskyi@masterofcode.com>
2013-10-30 10:06:25 +0200

    update controller

--- oop/Classes/Controller.php
+++ oop/Classes/Controller.php
@@ -2,6 +2,7 @@
 
 namespace Classes;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 abstract class Controller
@@ -17,30 +18,41 @@ abstract class Controller
 
     protected function render($view, $data)
     {
+        $data['controller'] = $this->object;
+
+        if (!empty($data) && is_array($data)) extract($data, EXTR_PREFIX_ALL, 'view');
+
         $viewsRoot = realpath(dirname(__FILE__) . "/../views") . '/';
 
         $layoutDir = $viewsRoot . 'layout/';
 
         $viewPath = $viewsRoot . $this->object . '/' . $view . '.php';
 
-        $view_controller = $this->object;
-
-        if (!empty($data) && is_array($data)) extract($data, EXTR_PREFIX_ALL, 'view');
+        ob_start();
 
         include $layoutDir . 'header.php';
         if (file_exists($viewPath)) include $viewPath;
         include $layoutDir . 'footer.php';
+
+        $viewContent = ob_get_contents();
+        ob_end_clean();
+
+        $response = new Response();
+        $response->setContent($viewContent);
+        $response->headers->set('Content-Type', 'text/html');
+
+        $response->send();
     }
 
     protected function renderPartial($partial, $data, $params = array())
     {
-        $viewPath = realpath(dirname(__FILE__) . "/../views") . '/' . $this->object . '/_' . $partial . '.php';
-
-        $part_controller = $this->object;
+        $data['controller'] = $this->object;
 
         if (!empty($data) && is_array($data)) extract($data, EXTR_PREFIX_ALL, 'part');
-        
+
         if (!empty($params) && is_array($params)) extract($params, EXTR_PREFIX_ALL, 'params');
+
+        $viewPath = realpath(dirname(__FILE__) . "/../views") . '/' . $this->object . '/_' . $partial . '.php';
 
         if (file_exists($viewPath)) include $viewPath;
     }
